f635483ae7274225efdca8dd6a2f7021f31feb60
harshith010 <harshithks94@gmail.com>
2023-10-01 22:25:33 +0530

    New commit

--- app.java
+++ app.java
@@ -2,7 +2,6 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Harshith turn on TV ---->");
                 System.out.println("Harshith turn on Mobile ---->");
-                System.out.println("Harshith turrrrrr on Mobile ---->");
 
 
     }
