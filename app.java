5d5a742d259a2539086cf228bce5d2ae7b7aee58
harshith010 <harshithks94@gmail.com>
2023-10-01 21:23:08 +0530

    Second commit

--- app.java
+++ app.java
@@ -2,6 +2,8 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Harshith turn on TV ---->");
                 System.out.println("Harshith turn on Mobile ---->");
+                System.out.println("Harshith turrrrrr on Mobile ---->");
+
 
     }
 }
